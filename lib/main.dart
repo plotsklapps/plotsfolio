62a0643376b4916fa9037a2235817f857124e066
plotsklapps <plotsklapps@gmail.com>
2024-07-01 18:50:14 +0200

    - Refactored.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:plotsfolio/firebase_options.dart';
 import 'package:plotsfolio/responsive_layout.dart';
-import 'package:plotsfolio/screens/calculator/calculator_screen.dart';
-import 'package:plotsfolio/screens/calculator/calculator_sidemenu.dart';
+import 'package:plotsfolio/screens/splashscreen/splash_screen.dart';
+import 'package:plotsfolio/screens/splashscreen/splash_sidemenu.dart';
 import 'package:plotsfolio/state/font_signal.dart';
 import 'package:signals/signals_flutter.dart';
 
@@ -42,8 +42,8 @@ class MainEntry extends StatelessWidget {
       // ResponsiveLayout needs a screen and screens larger than 840 pixels
       // will also show the side menu.
       home: const ResponsiveLayout(
-        screen: CalculatorScreen(),
-        sideMenu: CalculatorSideMenu(),
+        screen: SplashScreen(),
+        sideMenu: SplashScreenSideMenu(),
       ),
     );
   }
