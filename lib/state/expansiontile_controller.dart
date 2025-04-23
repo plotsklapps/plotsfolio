496bee2b776e843b0aa1f494fae384e59eb45a3b
plotsklapps <plotsklapps@gmail.com>
2025-04-23 14:43:07 +0000

    - Last commit for now.

--- lib/state/expansiontile_controller.dart
+++ lib/state/expansiontile_controller.dart
@@ -4,5 +4,5 @@ import 'package:signals/signals_flutter.dart';
 // The ExpansionTileController is used to control the expansion of the
 // ExpansionTile in the NotificationBar. It is a signal so it can be used
 // from swiping the homescreen.
-final Signal<ExpansionTileController> sExpansionTileController =
-    signal(ExpansionTileController(), debugLabel: 'sExpansionTileController');
+final Signal<ExpansibleController> sExpansionTileController =
+    signal(ExpansibleController(), debugLabel: 'sExpansionTileController');
